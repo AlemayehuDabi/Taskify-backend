1603cc1e38e39e7d52d3814ca0f3a17acb8f5efc
Alemayehu <alemayehudabi606@gmail.com>
2025-08-15 02:09:38 +0300

    basic api

---
+++ main.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID int `json:"id"`
+	Completed bool `json:"completed"`
+	Body string `json:"body"`
+}
+
+func main() {
+	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/", func (c *fiber.Ctx) error {
+		return 	c.Status(200).JSON(fiber.Map{"msg" : "successfully fetch todos", "todos" : todos})
+		})
+
+	// create a todo
+	app.Post("/api/todos", func (c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if(todo == &Todo{}){
+			return c.Status(401).JSON(fiber.Map{"error": "Todo is required"})
+		}
+
+		if (todo.Body == "") {
+			return c.Status(401).JSON(fiber.Map{"error": "Todo body is required"})
+		}
+
+		// increase id value
+		todo.ID = len(todos) + 1
+
+		// put the data in the todos array
+		todos = append(todos, *todo)
+
+		return c.Status(200).JSON(fiber.Map{"msg": "successfully", "todos": todos})
+	})
+
+	// mark as completed
+	app.Patch("/api/todos/:id", func (c *fiber.Ctx) error  {
+		id := c.Params("id")
+
+		if(id == ""){
+			return c.Status(401).JSON(fiber.Map{"error": "Todo ID is required in order to mark as completed",})
+		}
+
+		for i:= range todos {
+			if(fmt.Sprint(todos[i].ID) == id){
+				todos[i].Completed = true
+				return c.Status(200).JSON(fiber.Map{"msg": "successfully mark as completed", "todos": todos})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{
+			"error" : "unable to mark as completed"})
+		
+	})
+
+
+	// update a todo
+	app.Put("/api/todos/:id", func(c *fiber.Ctx) error {
+		id :=  c.Params("id")
+
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		if(todo == &Todo{}){
+			return c.Status(401).JSON(fiber.Map{"error": "Todo is required inorder to update"})
+		}
+
+		if(id == ""){
+			return c.Status(401).JSON(fiber.Map{"error": "Todo ID is required in order to update",})
+		}
+
+		if(todo.Body == ""){
+			return c.Status(401).JSON(fiber.Map{"error": "Todo Body is required in order to update",})
+		}
+
+		for i := range todos {
+			if (fmt.Sprint(todos[i].ID) == id){
+				todos[i].Completed = (*todo).Completed
+				todos[i].Body = (*todo).Body
+				return c.Status(200).JSON(fiber.Map{"msg": "successfully update a todo", "todos": todos})
+			}
+		} 
+
+		return c.Status(404).JSON(fiber.Map{
+			"error" : "unable to update"})
+
+	})
+
+
+	// delete a todo
+	app.Delete("/api/todos/:id", func(c *fiber.Ctx) error  {
+		id := c.Params("id")
+
+			if(id == ""){
+			return c.Status(401).JSON(fiber.Map{"error": "Todo ID is required in order to update",})
+		}
+
+		for i := range todos {
+			if(fmt.Sprint(todos[i]) == id){
+				todos = append(todos[: i], todos[i+1:]...)
+				return c.Status(200).JSON(fiber.Map{"msg": "successfully delete the todo", "todos": todos})
+			}
+		}
+
+		return c.Status(404).JSON(fiber.Map{
+			"error" : "unable to delete a todo"})
+		
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
